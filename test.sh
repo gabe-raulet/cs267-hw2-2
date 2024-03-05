936f7b855a7a61ab24bdae8eb8580bc46abea759
Gabriel Raulet <gabe.h.raulet@gmail.com>
2024-03-04 19:17:21 -0800

    slight change

--- test.sh
+++ test.sh
@@ -5,12 +5,16 @@ SRC_DIR=/Users/gabrielraulet/Dev/March2024/cs267-hw2-2
 CORRECTNESS_DIR=$SRC_DIR/hw2-correctness
 BUILD_DIR=$SRC_DIR/build
 
+mpirun -np 1 $BUILD_DIR/mpi -s 1 -n 1000 -o $BUILD_DIR/mpi.n1000.out
+#python $CORRECTNESS_DIR/correctness-check.py $BUILD_DIR/mpi.n1000.out $CORRECTNESS_DIR/verf.n1000.out
+rm $BUILD_DIR/mpi.n1000.out
+
 mpirun -np 4 $BUILD_DIR/mpi -s 1 -n 1000 -o $BUILD_DIR/mpi.n1000.out
-python $CORRECTNESS_DIR/correctness-check.py $BUILD_DIR/mpi.n1000.out $CORRECTNESS_DIR/verf.n1000.out
+#python $CORRECTNESS_DIR/correctness-check.py $BUILD_DIR/mpi.n1000.out $CORRECTNESS_DIR/verf.n1000.out
 rm $BUILD_DIR/mpi.n1000.out
 
 mpirun -np 9 $BUILD_DIR/mpi -s 1 -n 1000 -o $BUILD_DIR/mpi.n1000.out
-python $CORRECTNESS_DIR/correctness-check.py $BUILD_DIR/mpi.n1000.out $CORRECTNESS_DIR/verf.n1000.out
+#python $CORRECTNESS_DIR/correctness-check.py $BUILD_DIR/mpi.n1000.out $CORRECTNESS_DIR/verf.n1000.out
 rm $BUILD_DIR/mpi.n1000.out
 
 #srun -N 1 --ntasks-per-node=1 $BUILD_DIR/mpi -s 1 -n 5000 -o $BUILD_DIR/mpi.n5000.out
